abe12d5875c8862ce427ce75e2c74119411867b8
Khresterion (Jeremy) <axios.ludis@gmail.com>
2021-06-27 16:52:38 +0200

    chore: basic regex in contact page

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
@@ -19,24 +21,48 @@ class ContactType extends AbstractType
         $builder
             ->add('prenom',  TextType::class, [
                 'label' => false,
+                'constraints' => [
+                    new NotBlank(),
+                    new Length([
+                        'min' => 2,
+                        'max' => 50,
+                        'minMessage' => 'Vous devez avoir au moins {{ limit }} caractères de long.',
+                        'maxMessage' => 'Vous devez au maximum avoir {{ limit }} caractères de long.'
+                    ]),
+                ],
                 'attr' => [
                     'placeholder' => 'Votre prénom'
                 ]
             ])
             ->add('nom', TextType::class, [
                 'label' => false,
+                'constraints' => [
+                    new NotBlank(),
+                    new Length([
+                        'min' => 2,
+                        'max' => 50,
+                        'minMessage' => 'Vous devez avoir au moins {{ limit }} caractères de long.',
+                        'maxMessage' => 'Vous devez au maximum avoir {{ limit }} caractères de long.'
+                    ]),
+                ],
                 'attr' => [
                     'placeholder' => 'Votre nom'
                 ]
             ])
             ->add('email', EmailType::class, [
                 'label' => false,
+                'constraints' => [
+                    new NotBlank(),
+                ],
                 'attr' => [
                     'placeholder' => 'Votre Email'
                 ]
             ])
             ->add('content', TextareaType::class, [
                 'label' => false,
+                'constraints' => [
+                    new NotBlank(),
+                ],
                 'attr' => [
                     'placeholder' => 'Votre message'
                 ]
